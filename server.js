a9ecac31ddad135bca1b6166964012538130ccf7
Riyas OP <riyasbitz@gmail.com>
2016-10-28 11:59:05 +0530

    Added a ProcFile.

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function(request, response) {
-  response.send("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
