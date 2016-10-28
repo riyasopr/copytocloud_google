678628d2f48f5c0e86cf7004887aeade9c7ef820
Riyas OP <riyasbitz@gmail.com>
2016-10-28 12:26:17 +0530

    Added a ProcFile.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!'+app.get('port'))
 })
 
-  console.log("Node app is running at localhost:" + app.get('port'))
